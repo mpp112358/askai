101946bad4410e3cb747582604d4bb9be021fc9a
Manu <manuelperezpinar@gmail.com>
2024-12-24 19:54:49 +0100

    First working version

--- gemacs.el
+++ gemacs.el
@@ -9,7 +9,7 @@
 ;; Version: 0.0.1
 ;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
 ;; Homepage: https://github.com/manu/gemacs
-;; Package-Requires: ((emacs "24.3"))
+;; Package-Requires: ((emacs "29.1"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -19,16 +19,44 @@
 ;;
 ;;; Code:
 
-(defun count-chars ()
-  "Count characters written by the user"
-  (interactive)
-  (let ((buffer (get-buffer-create "count")))
-    (set-buffer buffer)
-    (switch-to-buffer buffer)
-    (insert "> ")
-    (evil-insert-state)
-    (call-process "python" "/home/manu/bin/count.py")))
+(defvar gemacs-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "RET") #'gemacs-run-prompt)
+    map)
+  "Keymap for gemacsm-mode.")
 
+(define-minor-mode gemacs-mode
+  "GEmacs mode for a Gemini prompt."
+  :lighter " GEM"
+  :keymap gemacs-mode-map
+  (if gemacs-mode
+      (progn
+        (message "GEmacs mode enabled"))
+    (message "GEmacs mode disabled")))
+
+(defun gemacs-run-prompt ()
+  "Get the last prompt and send it to Gemini."
+  (interactive)
+  (insert "\n")
+  (let ((start (+ (line-beginning-position 0) 2))
+        (end (line-end-position 0)))
+    (message (buffer-substring start end))
+    (message "%d %d" start end)
+    (when (> end start)
+      (message "Waiting for answer from Gemini...")
+      (call-process-region start end "python3.10" nil '(t nil) t "/home/manu/bin/gemacs.py")
+      (message (buffer-substring start end))
+      (insert "# "))))
+
+(defun gemacs-run()
+  "Message Gemini."
+  (interactive)
+  (with-current-buffer (get-buffer-create "gemacs")
+    (switch-to-buffer (current-buffer))
+    (markdown-mode)
+    (gemacs-mode 1)
+    (insert "# ")
+    (evil-insert-state)))
 
 (provide 'gemacs)
 ;;; gemacs.el ends here
--- gemacs.py
+++ gemacs.py
@@ -2,14 +2,15 @@
 
 import pathlib
 import textwrap
-from cryptography.fernet import Fernet
+import json
 
 import google.generativeai as genai
 
 import markdown
 
-with open(".secrets") as secrets:
-    GOOGLE_API_KEY = secrets.readline().rstrip()
+with open("config.json") as configFile:
+    config = json.load(configFile)
+    GOOGLE_API_KEY = config["api_key"]
 
 genai.configure(api_key=GOOGLE_API_KEY)
 
@@ -17,9 +18,6 @@ model = genai.GenerativeModel("gemini-1.5-flash")
 
 chat = model.start_chat(history=[])
 
-message = ""
-
-while not message == "bye":
-    message = input("> ")
-    response = chat.send_message(message)
-    print(response.text)
+message = input()
+response = chat.send_message(message)
+print(response.text)
